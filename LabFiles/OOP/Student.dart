0410f9dff2e0344265cafae199b20535f30aa4fb
youms <yussef.msabeh@gmail.com>
2022-06-07 14:03:18 +0300

    modified student class

--- LabFiles/OOP/Student.dart
+++ LabFiles/OOP/Student.dart
@@ -6,15 +6,14 @@ class Student {
   late Level level;
 
   String getStudentGpa() {
-    if (this.gpa >= 90) {
+    if (this.gpa >= 90)
       return "excellent";
-    } else if (this.gpa >= 80) {
+    else if (this.gpa >= 80)
       return "vgood";
-    } else if (this.gpa >= 70) {
+    else if (this.gpa >= 70)
       return "good";
-    } else if (this.gpa >= 60) {
-      return "poor";
-    }
+    else if (this.gpa >= 60) return "poor";
+
     return "failed";
   }
 }
