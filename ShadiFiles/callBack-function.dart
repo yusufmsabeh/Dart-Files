0ae8ede049c2645a8d34d98102395f68934e0125
youms <yussef.msabeh@gmail.com>
2022-06-22 21:22:13 +0300

    modified some files

--- ShadiFiles/callBack-function.dart
+++ ShadiFiles/callBack-function.dart
@@ -2,26 +2,26 @@
 typedef String GsgFunction(String x);
 
 void main(List<String> args) {
-  String capetilizeFirstLeter(String name) {
-    return name[0].toUpperCase() + name.substring(1);
-  }
-
-  String formatMiddleName(String name) {
-    return ' ' + name[0].toUpperCase() + '. ';
-  }
-
-  String? formatFullName(GsgFunction fun1, GsgFunction fun2, String fullName) {
-    List<String> names = fullName.split(' ');
-    if (names.length != 3) {
-      return null;
-    } else {
-      String firstName = fun1(names[0]);
-      String secondName = fun2(names[1]);
-      String lastName = fun1(names[2]);
-      return firstName + secondName + lastName;
-    }
-  }
-
   print(formatFullName(
       capetilizeFirstLeter, formatMiddleName, 'shady ahmed mohammed'));
+}
+
+String capetilizeFirstLeter(String name) {
+  return name[0].toUpperCase() + name.substring(1);
+}
+
+String formatMiddleName(String name) {
+  return ' ' + name[0].toUpperCase() + '. ';
+}
+
+String? formatFullName(GsgFunction fun1, GsgFunction fun2, String fullName) {
+  List<String> names = fullName.split(' ');
+  if (names.length != 3) {
+    return null;
+  } else {
+    String firstName = fun1(names[0]);
+    String secondName = fun2(names[1]);
+    String lastName = fun1(names[2]);
+    return firstName + secondName + lastName;
+  }
 }
